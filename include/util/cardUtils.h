83a4594600c5af78c51d3a0bfe5dfd097c2b8dc6
Isaac <iico7no2@gmail.com>
2022-09-03 16:47:53 -0500

    Refactor

--- include/util/cardUtils.h
+++ include/util/cardUtils.h
@@ -16,7 +16,26 @@
 
 namespace libtp::util::card
 {
-    int32_t GetDirEntries( int32_t chn, libtp::gc_wii::card::__DirEntry* dirEntries, int32_t* count, bool showall );
+    /**
+     *  @brief Gets the DirEntries for a memory card. Based on params, copies
+     *  data into dirEntries for each matching DirEntry. The number of copied
+     *  entries is written to `count`. Note this should be called after the
+     *  memory card has been mounted (at the same time you might call CARDOpen,
+     *  for example). This function is based off of CARD_GetDirectory from
+     *  https://github.com/devkitPro/libogc/blob/master/gc/ogc/card.h
+     *
+     *  @param chan EXI channel number (must be memory card slot A or B)
+     *  @param dirEntries pointer to a fresh __DirEntries. This will have enough
+     *  space to store data for the maximum of 127 files on the memory card.
+     *  @param count pointer to which the number of matching DirEntry is
+     *  written.
+     *  @param showAll If true, every DirEntry for any game on the memory card
+     *  will be copied to `dirEntries`. If false, will only copy ones which
+     *  match the publisherCode and gameCode of the game being played (for
+     *  example, DirEntry which start with "GZ2E01").
+     *  @return Code indicating success or reason for failure.
+     */
+    int32_t GetDirEntries( int32_t chan, libtp::gc_wii::card::__DirEntries* dirEntries, int32_t* count, bool showAll );
 
 }     // namespace libtp::util::card
 
--- source/util/cardUtils.cpp
+++ source/util/cardUtils.cpp
@@ -10,116 +10,48 @@ namespace libtp::util::card
     using libtp::gc_wii::card::__DirEntries;
     using libtp::gc_wii::card::__DirEntry;
 
-    /*! \typedef struct _card_file card_file
-    \brief structure to hold the fileinformations upon open and for later use.
-    \param chn CARD slot.
-    \param filenum file index in the card directory structure.
-    \param offset offset into the file.
-    \param len length of file.
-    \param iblock block index on memory card.
-    */
-    typedef struct _card_file
-    {
-        int32_t chn;
-        int32_t filenum;
-        int32_t offset;
-        int32_t len;
-        uint16_t iblock;
-    } card_file;
-
-    /*! \typedef void (*cardcallback)(s32 chan,s32 result)
-    \brief function pointer typedef for the user's operation callback
-    \param chan CARD slot
-    \param result result of operation upon call of callback.
-    */
-    typedef void ( *cardcallback )( int32_t chan, int32_t result );
-
-    // typedef struct _card_block
-    // {
-    //     uint8_t cmd[9];
-    //     uint32_t cmd_len;
-    //     uint32_t cmd_mode;
-    //     uint32_t cmd_blck_cnt;
-    //     uint32_t cmd_sector_addr;
-    //     uint32_t cmd_retries;
-    //     uint32_t attached;
-    //     int32_t result;
-    //     uint32_t cid;
-    //     uint16_t card_size;
-    //     uint32_t mount_step;
-    //     uint32_t format_step;
-    //     uint32_t sector_size;
-    //     uint16_t blocks;
-    //     uint32_t latency;
-    //     uint32_t cipher;
-    //     uint32_t key[3];
-    //     uint32_t transfer_cnt;
-    //     uint16_t curr_fileblock;
-    //     card_file* curr_file;
-    //     struct card_dat* curr_dir;
-    //     struct card_bat* curr_fat;
-    //     void* workarea;
-    //     void* cmd_usr_buf;
-    //     uint32_t wait_sync_queue;     // type is lwpq_t
-    //     uint32_t timeout_svc;         // type is syswd_t
-    //     uint8_t dsp_task[0x40];       // type is dsptask_t (struct, not an array)
-
-    //     cardcallback card_ext_cb;
-    //     cardcallback card_tx_cb;
-    //     cardcallback card_exi_cb;
-    //     cardcallback card_api_cb;
-    //     cardcallback card_xfer_cb;
-    //     cardcallback card_erase_cb;
-    //     cardcallback card_unlock_cb;
-    // } card_block;
-
-    typedef struct _card_block
+    // This is at least 0x88 bytes long since offset 0x84 is a pointer to the
+    // DirectoryEntry data, but we don't need the exact details since we only
+    // care about the first 8 bytes.
+    struct card_block
     {
         int32_t attached;
         int32_t cardResult;
-    } card_block;
-    // ^ This is at least 0x88 bytes long since offset 0x84 is a pointer to the
-    // DirectoryEntry data.
+    };
 
-    /*! \fn s32 CARD_GetDirectory(s32 chn, card_dir *dir_entries, s32 *count, bool showall)
-    \brief Returns the directory entries. size of entries is max. 128.
-    \param[in] chn CARD slot
-    \param[out] dir_entries pointer to card_dir structure to receive the result set.
-    \param[out] count pointer to an integer to receive the counted entries.
-    \param[in] showall Whether to show all files of the memory card or only those which are identified by the company and
-    gamecode string. \return \ref card_errors "card error codes"
-    */
-    int32_t GetDirEntries( int32_t chn, __DirEntry* dirEntries, int32_t* count, bool showall )
+    int32_t GetDirEntries( int32_t chan, __DirEntries* dirEntries, int32_t* count, bool showAll )
     {
         int32_t numMatches = 0;
         int32_t ret = CARD_RESULT_READY;
         card_block* cardBlock = nullptr;
 
-        if ( chn < CARD_SLOT_A || chn > CARD_SLOT_B )
+        if ( chan < CARD_SLOT_A || chan > CARD_SLOT_B )
             return CARD_RESULT_NOCARD;
 
-        ret = libtp::gc_wii::card::__CARDGetControlBlock( chn, (void**) &cardBlock );
+        ret = libtp::gc_wii::card::__CARDGetControlBlock( chan, (void**) &cardBlock );
         if ( ret < 0 )
             return ret;
 
         if ( cardBlock->attached )
         {
-            __DirEntries* dirblock = static_cast<__DirEntries*>( libtp::gc_wii::card::__CARDGetDirBlock( (void*) cardBlock ) );
-
+            __DirEntries* dirblock = libtp::gc_wii::card::__CARDGetDirBlock( (void*) cardBlock );
             __DirEntry* entries = dirblock->entries;
+            __DirEntry* outEntries = dirEntries->entries;
 
+            // For example, "GZ2E".
             char* card_gamecode = reinterpret_cast<char*>( 0x80000000 );
+            // For example, "01".
             char* card_company = reinterpret_cast<char*>( 0x80000004 );
 
             for ( int i = 0; i < CARD_MAX_FILE; i++ )
             {
                 if ( entries[i].gameCode[0] != 0xFF )
                 {
-                    if ( showall ||
+                    if ( showAll ||
                          ( ( card_gamecode[0] != 0xFF && memcmp( entries[i].gameCode, card_gamecode, 4 ) == 0 ) &&
                            ( card_company[0] != 0xFF && memcmp( entries[i].publisherCode, card_company, 2 ) == 0 ) ) )
                     {
-                        memcpy( (void*) &dirEntries[numMatches], (void*) &entries[i], sizeof( __DirEntry ) );
+                        memcpy( (void*) &outEntries[numMatches], (void*) &entries[i], sizeof( __DirEntry ) );
                         numMatches += 1;
                     }
                 }
