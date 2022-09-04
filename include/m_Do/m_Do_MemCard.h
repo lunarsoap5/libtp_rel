4c7c03fe33c9dbf744621d97b372fdd7b2d7351b
Isaac <iico7no2@gmail.com>
2022-09-03 21:08:21 -0500

    Update loadfile to match decomp

--- include/m_Do/m_Do_MemCard.h
+++ include/m_Do/m_Do_MemCard.h
@@ -45,8 +45,7 @@ namespace libtp::m_Do_MemCard
     extern "C"
     {
         void mDoMemCd_Ctrl_c__detach( mDoMemCd_Ctrl_c* _this );
-        // Decomp says this function returns an s32. Need to confirm with the map.
-        bool mDoMemCd_Ctrl_c__loadfile( mDoMemCd_Ctrl_c* _this );
+        int32_t mDoMemCd_Ctrl_c__loadfile( mDoMemCd_Ctrl_c* _this );
     }
 
 }     // namespace libtp::m_Do_MemCard
